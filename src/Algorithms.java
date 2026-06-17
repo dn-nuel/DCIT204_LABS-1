c712dd16476210a0ad3b3498ebd987fd257a4819
Emmanuel Asante <easante268@st.ug.edu.gh>
2026-06-17 10:20:42 +0000

    linear and binary search algorithms

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,31 @@
 public class Algorithms {
+
+
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;   
+            }
+        }
+        return -1;      
+    }
+
+    
+    public static int binarySearch(int[] array, int target) {
+        int low  = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;  
+
+            if (array[mid] == target) {
+                return mid;                      
+            } else if (array[mid] < target) {
+                low = mid + 1;                   
+            } else {
+                high = mid - 1;                
+            }
+        }
+        return -1;                               
+    }
 }
